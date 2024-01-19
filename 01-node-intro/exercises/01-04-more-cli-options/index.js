32b91403729594975b6da2aef8cd9cd91a9651ca
nataponj <nataponj@gmail.com>
2024-01-19 13:54:10 +0700

    finish 01-04

--- 01-node-intro/exercises/01-04-more-cli-options/index.js
+++ 01-node-intro/exercises/01-04-more-cli-options/index.js
@@ -5,9 +5,19 @@ const filePath = "data.txt";
 const option = process.argv[2];
 const input = process.argv[3];
 
+
 if (option === "w") {
   fs.appendFileSync(filePath, input + "\n");
   console.log("appended:", input);
-} else {
+} else if (option === 'r') {
+  const read = fs.readFileSync(filePath, 'utf-8')
+  console.log(read)
+} else if (option === 'c') {
+  fs.truncateSync(filePath)  
+  //or 
+//  fs.writeFileSync(filePath, '')
+console.log('clear content in +', filePath)
+}
+else {
   console.log("invalid option");
 }
