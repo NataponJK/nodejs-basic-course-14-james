1920dece6b82e1234e3df8e5e012ad16bd369bf8
nataponj <nataponj@gmail.com>
2024-01-22 22:37:52 +0700

    finish 03-03

--- 03-express-intro/exercises/03-03-express-routing-with-data/app.js
+++ 03-express-intro/exercises/03-03-express-routing-with-data/app.js
@@ -11,14 +11,28 @@ const userDatabase = {
   22: "Mano",
 };
 
-app.get("/users/:userId", (req, res) => {
+app.get("/users/:userId(\\d+)", (request, response) => {
   // 1. Implement: returns "User ID: [NUMBER] (name: [NAME])"
+  console.log(request.params);
+  const id = request.params.userId;
+  const name = userDatabase[id];
+  response.send(`User Id: ${id} (name: ${name})`);
 });
 
-app.post("/users/:userId", (req, res) => {
+app.post("/users/:userId(\\d+)", (request, response) => {
   // 2. Implement: returns "User ID: [NUMBER], name has been updated to [NAME]".
+  console.log(request.params);
+  const id = request.params.userId;
+  const name = userDatabase[id];
+  
+  if(!userDatabase[id]) {
+    response.send(`Error User ID: ${id} not found.`)
+  }
+
+  newName = request.body.name;
+  response.send(`User ID: ${id}, ${name} has been up date to ${newName}`);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port http://localhost:${port}`);
 });
