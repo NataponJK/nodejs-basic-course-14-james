e8113d63adeae8d2f628d9c6389d308c41a8b735
nataponj <nataponj@gmail.com>
2024-01-22 09:43:26 +0700

    add utils 01-05

--- 01-node-intro/exercises/01-05-refactor-fn-import/utils.js
+++ 01-node-intro/exercises/01-05-refactor-fn-import/utils.js
@@ -1,18 +1,18 @@
-import fs from 'fs'
+import fs from "fs";
 
 const filePath = "data.txt";
 
 export function write(input) {
-    fs.appendFileSync(filePath, input + "\n");
-    console.log("appended:", input);
-  }
-  
+  fs.appendFileSync(filePath, input + "\n");
+  console.log("appended:", input);
+}
+
 export function read() {
   const content = fs.readFileSync(filePath, "utf-8");
   console.log("file content:", content);
 }
 
 export function clear() {
-    fs.writeFileSync(filePath, "");
-    console.log("the file has been clear");
-  }
+  fs.writeFileSync(filePath, "");
+  console.log("the file has been clear");
+}
