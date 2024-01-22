a26fa3b4200504e522076ee06ddb81a1e6096e38
nataponj <nataponj@gmail.com>
2024-01-22 15:42:15 +0700

    finished 03-02 without challenge

--- 03-express-intro/exercises/03-02-express-routing/app.js
+++ 03-express-intro/exercises/03-02-express-routing/app.js
@@ -2,22 +2,39 @@ import express from "express";
 const app = express();
 const port = 8000;
 
-app.get("/users", (req, res) => {
-  // 1. Implement: returns "Users ID: [NUMBER]", ex. "User ID: 20"
-});
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
 
-app.post("/users", (req, res) => {
-  // 2. Implement: returns "User ID: [NUMBER], name has been updated to [NAME]".
-});
+// app.get("/users", (reqeust, response) => {
+//   // 1. Implement: returns "Users ID: [NUMBER]", ex. "User ID: 20"
+//   console.log(reqeust.query)
+//   const id = reqeust.query.id
+//   response.send(`User ID: ${id}`)
+// });
 
-app.get("/users/:userId", (req, res) => {
+// app.post("/users", (reqeust, response) => {
+//   // 2. Implement: returns "User ID: [NUMBER], name has been updated to [NAME]".
+//   console.log(reqeust.body)
+//   const name = reqeust.body.name
+//   const id = reqeust.query.id
+//   response.send(`User ID: ${id}, name has been updated to ${name}`)
+// });
+
+app.get("/users/:userId", (reqeust, response) => {
   // 3. Implement: returns "Users ID: [NUMBER]", ex. "User ID: 20"
+  console.log(reqeust.params)
+  const id = reqeust.params.userId
+  response.send(`User ID:${id}`)
 });
 
-app.post("/users/:userId", (req, res) => {
+app.post("/users/:userId", (reqeust, response) => {
   // 4. Implement: returns "User ID: [NUMBER], name has been updated to [NAME]".
+  console.log(reqeust.body)
+  const name = reqeust.body.name
+  const id = reqeust.params.userId
+  response.send(`User ID: ${id}, name has been updated to ${name}`)
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port "http://localhost:${port}`);
 });
